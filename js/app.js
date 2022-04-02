9d71d2a9fe02a03d549b258db91070c950cf8168
CROGuru <63576231+CROGuru@users.noreply.github.com>
2022-04-02 18:51:15 +1100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+	$('a.js-scroll-trigger:not([href="#"])').click(function (s) {
+		s.preventDefault();
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				window.smoothScroll(target.offset().top - 40, 800)
+			}
+		}
+	});
+	$('.js-scroll-trigger').click(function () {
+		$('.navbar-collapse').collapse('hide');
+	});
+	new bootstrap.ScrollSpy(document.body, {
+		target: '#menu',
+		offset: 110
+	});
+	var navbarCollapse = function () {
+		if ($("#menu").offset().top > 100) {
+			$("#menu").addClass("navbar-shrink");
+		} else {
+			$("#menu").removeClass("navbar-shrink");
+		}
+	};
+	navbarCollapse();
+	$(window).scroll(navbarCollapse);
+})(jQuery);
+AOS.init({once:true});
